b5c20cb1e78c468ec81dae633323f89758e8b65f
daiki saito <daiki.saito92@gmail.com>
2022-04-21 11:14:20 +0900

    update header styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,31 +6,65 @@ type Props = {}
 const Header = (props: Props) => {
   return (
     <StyledHeader>
-      <HeaderLeft>
-        <StyledLogo>
-          <a href="/">
-            <img src="images/logo.png" alt="logo" />
-          </a>
-        </StyledLogo>
-        <HeaderMenu>
-          <a href="/lottery">Lottery</a>
-          <a href="/lottery-admin">Lottery Admin</a>
-        </HeaderMenu>
-      </HeaderLeft>
-      <ConnectWallet isHeaderBtn={true} />
+      <StyledHeaderContent>
+        <HeaderLeft>
+          <HeaderLogo>
+            <a href="/">
+              <img src="images/logo.png" alt="logo" />
+            </a>
+            ZEUS Node
+          </HeaderLogo>
+        </HeaderLeft>
+        <HeaderRight>
+          <HeaderMenu>
+            <a href="/lottery">Lottery</a>
+            <a href="/lottery-admin">Lottery Admin</a>
+          </HeaderMenu>
+          <HeaderAction>
+            <ConnectWallet isHeaderBtn={true} />
+          </HeaderAction>
+        </HeaderRight>
+      </StyledHeaderContent>
     </StyledHeader>
   )
 }
 
+const StyledHeader = styled.div`
+  padding: 22px 8px;
+  boxshadow: 2px 2px 5px rgb(0 0 0 / 20%);
+  background: #2e3c67;
+`
+
+const StyledHeaderContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 36px;
+  max-width: 1200px;
+  margin: 0 auto;
+`
+
+// left
 const HeaderLeft = styled.div`
   display: flex;
 `
 
-const StyledLogo = styled.div`
+const HeaderLogo = styled.div`
+  display: flex;
+  align-items: center;
   height: 100%;
   img {
-    height: 100%;
+    width: 64px;
   }
+  color: #fff;
+  font-size: 18px;
+  font-weight: 600;
+`
+
+// right
+const HeaderRight = styled.div`
+  display: flex;
+  align-items: center;
 `
 
 const HeaderMenu = styled.div`
@@ -38,17 +72,19 @@ const HeaderMenu = styled.div`
   display: flex;
   align-items: center;
   a {
-    margin-left: 30px;
+    color: #fff;
+    margin-right: 20px;
+    &:hover {
+      color: #c4c4c4;
+    }
   }
 `
 
-const StyledHeader = styled.div`
-  padding: 8px;
-  height: 56px;
-  display: flex;
-  justify-content: space-between;
-  boxshadow: 2px 2px 5px rgb(0 0 0 / 20%);
-  background: #2e3c67;
+const HeaderAction = styled.div`
+  height: 48px;
+  button {
+    height: 100%;
+  }
 `
 
 export default Header
