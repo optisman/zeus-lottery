c24076b95c4b7fb649121e24fc17299826d5c6b4
daiki saito <daiki.saito92@gmail.com>
2022-04-29 00:23:28 +0900

    update title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>LOTTERY - ZEUS FINANCE</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
